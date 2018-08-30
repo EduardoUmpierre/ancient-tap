ce264539317ce59e2bdc9eefb99d581de46b0eab
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2018-08-30 13:04:55 -0300

    Unnecessary variables removed

--- Assets/Scripts/BossTimer.cs
+++ Assets/Scripts/BossTimer.cs
@@ -6,19 +6,13 @@ using UnityEngine.UI;
 public class BossTimer : MonoBehaviour {
     public GameObject timerObject;
 
-    GameObject monster;
-    Monster monsterComponent;
     Text timer;
     Hero hero;
     float timeLeft = 20.0f;
-    int milliseconds;
 
     // Use this for initialization
     void Start ()
     {
-        monster = GameObject.FindGameObjectsWithTag("Enemy")[0];
-        monsterComponent = monster.GetComponent<Monster>();
-
         timer = timerObject.GetComponent<Text>();
         hero = GameObject.Find("Hero").GetComponent<Hero>();
     }
@@ -27,7 +21,6 @@ public class BossTimer : MonoBehaviour {
 	void Update ()
     {
         timeLeft -= Time.deltaTime;
-        milliseconds = (int)(timeLeft * 1000f) % 1000;
 
         timer.text = timeLeft.ToString("N2");
 
